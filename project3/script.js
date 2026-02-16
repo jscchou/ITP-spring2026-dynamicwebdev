9e1568b04ac225f5491eaf4aea0bf9ef75ef1ff4
Jasmine Chou <jasminechou@mac.myfiosgateway.com>
2026-02-16 02:01:43 -0500

    updated on 2/15

--- project3/script.js
+++ project3/script.js
@@ -44,14 +44,14 @@ async function showWeatherPopup(locationName) {
     currentTempText.innerText = "Loading...";
 
     const params = new URLSearchParams({
-        access_key: "8d1ec3197fefddc8db850f991fcf9db0",
-        query: locationName + ", Taiepi, Taiwan",
+        key: "be1185548edf4ee589962751261602",
+        q: locationName + ", Taiepi, Taiwan",
         units: "m"
     });
 
     try {
       
-        const response = await fetch("http://api.weatherstack.com/current?" + params);
+        const response = await fetch("https://api.weatherapi.com/v1/current.json?" + params);
         const jsonData = await response.json();
 
 
@@ -62,9 +62,19 @@ async function showWeatherPopup(locationName) {
         }
 
         if (jsonData.current) {
-            const temp = jsonData.current.temperature;
-            const desc = jsonData.current.weather_descriptions[0];
-            currentTempText.innerHTML = `Currently: <strong>${temp}°C</strong><br>${desc}`;
+            const day = jsonData.current.is_day === 1 ? "Day" : "Night";
+            const temp = jsonData.current.temp_c;
+            const feelsliketemp = jsonData.current.feelslike_c;
+            const humidity = jsonData.current.humidity;
+            const time = jsonData.current.last_updated;
+
+            currentTempText.innerHTML = `Time of Day: ${day}<br>
+            Currently: ${temp}°C<br>
+            Feels like: ${feelsliketemp}°C<br>
+            Humidity: ${humidity}%<br>
+            Last Updated: ${time}<br>
+            `;
+    
         } else {
             currentTempText.innerText = "Location not found.";
         }
